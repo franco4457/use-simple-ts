a03481744cc096f5ae85c454cfee32b612043063
franco4457 <franco_forex@hotmail.com>
2023-06-24 17:11:05 -0300

    add absolute paths to configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
+  server: {
     port: 3000,
-  }
+  },
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' },
+      { find: '@components', replacement: '/src/components' },
+    ],
+  },
 })
