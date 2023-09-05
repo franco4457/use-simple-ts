4009352c84442e5a11bb49e4b18e343e5ab475f0
Franco de Desarrollo <francoegi01@gmail.com>
2023-09-05 20:29:37 -0300

    feat(hooks/debounce): :test_tube: migrate test testing library

--- packages/hooks/use-debounce-ts/__tests__/use-debounce-ts.test.tsx
+++ packages/hooks/use-debounce-ts/__tests__/use-debounce-ts.test.tsx
@@ -1,11 +1,65 @@
-import { renderHook } from '@testing-library/react'
+import { act, renderHook, waitFor } from '@testing-library/react'
 
 import { useDebounceTS } from '../src'
+import { useState } from 'react'
 
 describe('useDebounceTs', () => {
   it('should work correctly', () => {
-    const { result } = renderHook(() => useDebounceTS(() => undefined, 1000))
+    const { result } = renderHook(() => useDebounceTS(() => undefined))
+    expect(result.current).toBeInstanceOf(Function)
+  })
+  it('should be debounce simple functions', async () => {
+    let count = 0
+    const fn = () => {
+      count++
+    }
+    const { result } = renderHook(() => useDebounceTS(fn, 500))
+    act(() => {
+      result.current()
+    })
+    expect(count).toBe(0)
+    await waitFor(
+      () => {
+        expect(count).toBe(1)
+      },
+      { timeout: 1000 }
+    )
+  })
 
-    // Add your test here
+  it('should be debounce functions that affects states', async () => {
+    const { result: counter } = renderHook(() => {
+      const [count, setCount] = useState(0)
+      const add = () => setCount((c) => c + 1)
+      return { count, add }
+    })
+    const { result } = renderHook(() => useDebounceTS(() => act(() => counter.current.add()), 500))
+    act(() => {
+      result.current()
+    })
+    expect(counter.current.count).toBe(0)
+    await waitFor(
+      () => {
+        expect(counter.current.count).toBe(1)
+      },
+      { timeout: 1000 }
+    )
+  })
+  it('should be debounce functions that affects states 2', async () => {
+    const { result } = renderHook(() => {
+      const [state, setState] = useState('aaa')
+      const update = () => setState(() => 'bbb')
+      return { state, update }
+    })
+    const debounce = renderHook(() => useDebounceTS(() => act(() => result.current.update()), 500))
+    act(() => {
+      debounce.result.current()
+    })
+    expect(result.current.state).toBe('aaa')
+    await waitFor(
+      () => {
+        expect(result.current.state).toBe('bbb')
+      },
+      { timeout: 1000 }
+    )
   })
 })
