c8ddd5d99c1d1fcc3cb728be8595b94b0e9936fa
Franco de Desarrollo <francoegi01@gmail.com>
2023-09-07 19:05:10 -0300

    test(hooks/debounce): :test_tube: cancel and flush utilities

--- packages/hooks/use-debounce-ts/__tests__/use-debounce-ts.test.tsx
+++ packages/hooks/use-debounce-ts/__tests__/use-debounce-ts.test.tsx
@@ -4,16 +4,16 @@ import { useDebounceTS } from '../src'
 import { useState } from 'react'
 
 describe('useDebounceTs', () => {
-  it('should work correctly', () => {
+  it.concurrent('should work correctly', () => {
     const { result } = renderHook(() => useDebounceTS(() => undefined))
     expect(result.current).toBeInstanceOf(Function)
   })
-  it('should be debounce simple functions', async () => {
+  it.concurrent('should be debounce simple functions', async () => {
     let count = 0
     const fn = () => {
       count++
     }
-    const { result } = renderHook(() => useDebounceTS(fn, 500))
+    const { result } = renderHook(() => useDebounceTS(fn, 100))
     act(() => {
       result.current()
     })
@@ -61,5 +61,177 @@ describe('useDebounceTs', () => {
       },
       { timeout: 1000 }
     )
+  })
+  it.concurrent('should debunce with multiple calls', async () => {
+    let count = 0
+    const fn = () => {
+      count++
+    }
+    const { result } = renderHook(() => useDebounceTS(fn, 100))
+    act(() => {
+      result.current()
+      result.current()
+      result.current()
+      result.current()
+    })
+    expect(count).toBe(0)
+    await waitFor(
+      () => {
+        expect(count).toBe(1)
+      },
+      { timeout: 1000 }
+    )
+  })
+  it.concurrent('should be debounce with multiple calls and multiple times', async () => {
+    let count = 0
+    const fn = () => {
+      count++
+    }
+    const { result } = renderHook(() => useDebounceTS(fn, 100))
+    act(() => {
+      result.current()
+      result.current()
+      result.current()
+      result.current()
+    })
+    expect(count).toBe(0)
+    await waitFor(
+      () => {
+        expect(count).toBe(1)
+      },
+      { timeout: 1000 }
+    )
+    act(() => {
+      result.current()
+      result.current()
+      result.current()
+      result.current()
+    })
+    expect(count).toBe(1)
+    await waitFor(
+      () => {
+        expect(count).toBe(2)
+      },
+      { timeout: 1000 }
+    )
+  })
+  it.concurrent('should be cancel debounce when call cancel', async () => {
+    let count = 0
+    const fn = () => {
+      count++
+    }
+    const { result } = renderHook(() => useDebounceTS(fn, 200))
+    act(() => {
+      result.current()
+      result.current()
+      result.current()
+      result.current.cancel()
+    })
+    expect(count).toBe(0)
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        expect(count).toBe(0)
+        resolve(0)
+      }, 1000)
+    })
+  })
+  it.concurrent('should be cancel debounce when call cancel 2', async () => {
+    let count = 0
+    const fn = () => {
+      count++
+    }
+    const { result } = renderHook(() => useDebounceTS(fn, 200))
+    act(() => {
+      result.current()
+      result.current()
+      result.current()
+      result.current.cancel()
+    })
+    expect(count).toBe(0)
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        expect(count).toBe(0)
+        resolve(0)
+      }, 1000)
+    })
+    act(() => {
+      result.current()
+      result.current()
+      result.current()
+      result.current()
+    })
+    expect(count).toBe(0)
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        expect(count).toBe(1)
+        resolve(0)
+      }, 1000)
+    })
+  })
+  it.concurrent('should be flush debounce when call flush', async () => {
+    let count = 0
+    const fn = () => {
+      count++
+    }
+    const { result } = renderHook(() => useDebounceTS(fn, 200))
+    act(() => {
+      result.current()
+      result.current()
+      result.current()
+      result.current.flush()
+    })
+    expect(count).toBe(1)
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        expect(count).toBe(1)
+        resolve(0)
+      }, 1000)
+    })
+  })
+  it.concurrent('should be flush debounce when call multiples flush', async () => {
+    let count = 0
+    const fn = () => {
+      count++
+    }
+    const { result } = renderHook(() => useDebounceTS(fn, 200))
+    act(() => {
+      result.current()
+      result.current()
+      result.current()
+      result.current.flush()
+    })
+    expect(count).toBe(1)
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        expect(count).toBe(1)
+        resolve(0)
+      }, 1000)
+    })
+    act(() => {
+      result.current()
+      result.current()
+      result.current()
+      result.current.flush()
+    })
+    expect(count).toBe(2)
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        expect(count).toBe(2)
+        resolve(0)
+      }, 1000)
+    })
+    act(() => {
+      result.current()
+      result.current.flush()
+      result.current.flush()
+      result.current.flush()
+    })
+    expect(count).toBe(5)
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        expect(count).toBe(5)
+        resolve(0)
+      }, 1000)
+    })
   })
 })
