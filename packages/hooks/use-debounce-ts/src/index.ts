09be6d51aa633fe16fea7e331dad00dc3a8e4ae1
Franco de Desarrollo <francoegi01@gmail.com>
2023-09-07 19:04:06 -0300

    feat(hook/debounce): add cancel and fluch utilities

--- packages/hooks/use-debounce-ts/src/index.ts
+++ packages/hooks/use-debounce-ts/src/index.ts
@@ -1,18 +1,28 @@
 import { useRef } from 'react'
 /* eslint-disable @typescript-eslint/ban-types  */
-type ArgumentTypes<F extends Function> = F extends (...args: infer A) => unknown
-  ? A
-  : never
+type ArgumentTypes<F extends Function> = F extends (...args: infer A) => unknown ? A : never
 
 export const useDebounceTS = <T extends Function>(callback: T, delay = 0) => {
   const debouceRef = useRef<ReturnType<typeof setTimeout>>()
-  return (...args: ArgumentTypes<T>): void => {
+
+  const debouncer = (...args: ArgumentTypes<T>): void => {
     if (debouceRef.current) clearTimeout(debouceRef.current)
     debouceRef.current = setTimeout(() => {
       // eslint-disable-next-line n/no-callback-literal
       return callback(...args)
     }, delay)
   }
+  debouncer.cancel = () => {
+    if (debouceRef.current) clearTimeout(debouceRef.current)
+  }
+  debouncer.flush = (...args: ArgumentTypes<T>) => {
+    if (debouceRef.current) {
+      clearTimeout(debouceRef.current)
+      // eslint-disable-next-line n/no-callback-literal
+      return callback(...args)
+    }
+  }
+  return debouncer
 }
 
 export type UseDebounceTsReturn = ReturnType<typeof useDebounceTS>
